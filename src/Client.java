ee681f2abcec52deb6c6e25df8ed649ce836efd3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-05-03 15:38:55 -0300

    add the adapter design pattern example

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+public class Client {
+    public static void main(String[] args) {
+        MallardDuck mallardDuck = new MallardDuck();
+
+        WildTurkey wildTurkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(wildTurkey);
+        System.out.println("The turkey says ...");
+        wildTurkey.gobble();
+        wildTurkey.fly();
+
+        System.out.println("\nThe duck says ...");
+        testDuck(mallardDuck);
+
+        System.out.println("\nThe Turkey Adapter says ...");
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
